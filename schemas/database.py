5098b565df2aae9371bb42c91257a1a1f8c52908
imisi99 <isongrichard234@gmail.com>
2024-01-15 13:33:47 +0100

    Updating database

--- schemas/database.py
+++ schemas/database.py
@@ -4,5 +4,5 @@ from sqlalchemy.orm import declarative_base
 
 databases = 'sqlite:///user.sqlite'
 engine = create_engine(databases, connect_args={"check_same_thread": False})
-begin = sessionmaker(bind=engine, autocommit = False, autoflush="False")
+begin = sessionmaker(bind=engine, autocommit = False, autoflush=False)
 data = declarative_base() 
